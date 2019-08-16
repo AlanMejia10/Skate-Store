ecb3389fea31da56e4bc0ac696e535b80b1b074c
Alan Mejia <alan-mejia@outlook.com>
2019-08-16 17:55:58 -0500

    events section completed

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-TweenMax.from(".navigation", 1.2, {
+TweenMax.from(".navigation", 1, {
     y: "-100%",
     clearProps: 'all'
 });
 
 TweenMax.from(".header-info h1", 1, {
+    x: -100,
     opacity: 0,
     delay: 1.2
 });
 
 TweenMax.from(".header-info p", 1, {
+    x:100,
     opacity: 0,
-    delay: 1.6
+    delay: 1.5
 });
 
 TweenMax.from(".header-info a", 1, {
+    y:100,
     opacity: 0,
-    delay: 2
+    delay: 1.7
 });
+
+
+
